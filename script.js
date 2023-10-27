5b4982c65f07aacbf4b9b2d1c0de89f1b24aaab2
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-27 23:51:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+let btnnxt = document.getElementById("btnnxt");
+let btnprv = document.getElementById("btnprv");
+
+let c1 = document.getElementById("circle-1");
+let div = document.getElementById("div");
+
+let i=1;
+function hell() {
+	div.children[i].classList.add("active");
+	i++;
+	if(i<=2){
+	btnprv.disabled = false;
+}
+	if(i==5){
+	btnnxt.disabled = true;
+	}else{
+	}
+}
+function hell0000() {
+	if(i==2){
+	btnprv.disabled = true;
+	
+	}
+	i--;
+	btnnxt.disabled = false;
+	
+	div.children[i].classList.remove("active");
+	
+}
